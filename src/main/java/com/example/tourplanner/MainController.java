aea5dd163ee857036258ac545cfd7dca0023b75d
notbasem <bhamid@live.at>
2022-03-19 11:59:08 +0100

    Basic layout created

---
+++ src/main/java/com/example/tourplanner/MainController.java
@@ -0,0 +1,9 @@
+package com.example.tourplanner;
+
+import com.example.tourplanner.vm.MainVM;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainController {
+    private MainVM mainVM;
+}
---
+++ src/main/java/com/example/tourplanner/vm/MainVM.java
@@ -0,0 +1,12 @@
+package com.example.tourplanner.vm;
+
+public class MainVM {
+    // Sub-VMs inside of MainVM
+    // SearchVM searchVM;
+
+    /*
+    public MainVM(SearchVM searchVM) {
+        this.searchVM = searchVM;
+    }
+    */
+}
