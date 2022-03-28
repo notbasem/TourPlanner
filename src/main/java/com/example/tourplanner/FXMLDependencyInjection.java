326291132467d009cbad9a83aef5d177e9a89b42
notbasem <bhamid@live.at>
2022-03-28 17:30:23 +0200

    Layout improved and FXMLDI used

--- src/main/java/com/example/tourplanner/FXMLDependencyInjection.java
+++ src/main/java/com/example/tourplanner/FXMLDependencyInjection.java
@@ -1,30 +1,26 @@
 package com.example.tourplanner;
 
+import com.example.tourplanner.view.factory.ControllerFactory;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.Parent;
+
+import java.io.IOException;
+import java.util.Locale;
+
 public class FXMLDependencyInjection {
-    //BIG QUESTIONMARK
-    /*
-
- <><<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
-                                                         _________________
-                                                        |                 |
-                                                        |_ _______________|
-                                                          //           \\
-                                                         // -#-     -#- \\
-                                                        //    |_____|    \\
-                                                        \\_______________//
-                                                                 |
-                                                             \   |   /
-                                                              \  |  /
-                                                               \ | /
-                                                                \|/
-                                                                / \
-                                                               /   \
-                                                              /     \
-                                                             /       \
-                                                           ---       ---
-
-<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+    public static Parent load(String fxml, Locale locale) throws IOException {
+        FXMLLoader loader = getLoader(fxml, locale);
+        return loader.load();
+    }
 
 
-     */
+    private static FXMLLoader getLoader(String fxml, Locale locale) {
+        return new FXMLLoader(
+                FXMLDependencyInjection.class.getResource("/com/example/tourplanner/" + fxml),
+                null,
+                new JavaFXBuilderFactory(),
+                clazz -> ControllerFactory.getInstance().create(clazz)
+        );
+    }
 }
--- src/main/java/com/example/tourplanner/TourPlannerApplication.java
+++ src/main/java/com/example/tourplanner/TourPlannerApplication.java
@@ -2,19 +2,22 @@ package com.example.tourplanner;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Locale;
 
 public class TourPlannerApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(TourPlannerApplication.class.getResource("MainWindow.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        //FXMLLoader fxmlLoader = new FXMLLoader(TourPlannerApplication.class.getResource("MainWindow.fxml"));
+        Parent root = FXMLDependencyInjection.load("MainWindow.fxml", Locale.GERMAN);
+        Scene scene = new Scene(root);
         stage.setTitle("Tour Planner");
         stage.setScene(scene);
-       // stage.setFullScreen(true);
+        // stage.setFullScreen(true);
         stage.show();
     }
 
