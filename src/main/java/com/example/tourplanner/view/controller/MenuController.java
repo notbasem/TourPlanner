968b2b86b25f8648d5a0c35346dbc06ba44802ca
notbasem <bhamid@live.at>
2022-05-15 18:37:06 +0200

    Menu changed and Pdf-Export improved

--- src/main/java/com/example/tourplanner/view/controller/MenuController.java
+++ src/main/java/com/example/tourplanner/view/controller/MenuController.java
@@ -1,57 +1,70 @@
 package com.example.tourplanner.view.controller;
 
 import com.example.tourplanner.DAL.model.Tour;
-import com.example.tourplanner.business.TourManager;
 import com.example.tourplanner.viewmodel.MenuVM;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.properties.HorizontalAlignment;
 import com.itextpdf.layout.properties.UnitValue;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.io.FileNotFoundException;
-import java.util.EventListener;
 
 public class MenuController {
     private final MenuVM menuViewModel;
-    @FXML Button FileButton;
+    @FXML MenuBar menuBar;
+    @FXML MenuItem closeButton;
+    @FXML MenuItem exportButton;
     @FXML TextField searchTextField;
-    @FXML Button searchButton;
 
     public MenuController (MenuVM menuViewModel)
     {
         this.menuViewModel = new MenuVM() ;
     }
 
-    public void onFileButtonClick(){
+    @FXML
+    void initialize() {
+        searchTextField.textProperty().bindBidirectional(menuViewModel.searchStringProperty() );
+        searchTextField.textProperty().addListener((observable, oldValue, newValue) ->  menuViewModel.onSearch());
+    }
+
+    @FXML
+    public void onExport(){
         System.out.println("File export button was clicked");
         ObservableList<Tour> tourlist = this.menuViewModel.exportallTours();
         PdfWriter writer = null;
         try {
-            writer = new PdfWriter("target.pdf");
+            writer = new PdfWriter("tours.pdf");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
         PdfDocument pdf = new PdfDocument(writer);
         Document document = new Document(pdf);
 
-        for (Tour tour: tourlist
-             ) {
-            Table table = new Table(UnitValue.createPercentArray(8));
-            table.addHeaderCell("Name");
-            table.addHeaderCell("Description");
-            table.addHeaderCell("From");
-            table.addHeaderCell("To");
-            table.addHeaderCell("Transport Type");
-            table.addHeaderCell("Distance");
-            table.addHeaderCell("estimated Time");
-            table.addHeaderCell("Route Information");
+        Paragraph header = new Paragraph("Tours:");
+        header.setFontSize(32);
+        document.add(header);
 
+        // Creating a table
+        Table table = new Table(8);
+        table.addHeaderCell("Name");
+        table.addHeaderCell("Description");
+        table.addHeaderCell("From");
+        table.addHeaderCell("To");
+        table.addHeaderCell("Transport type");
+        table.addHeaderCell("Distance");
+        table.addHeaderCell("Estimated time");
+        table.addHeaderCell("Route information");
+
+        for (Tour tour: tourlist) {
             table.addCell(tour.getName());
             table.addCell(tour.getTourDescription());
             table.addCell(tour.getFrom());
@@ -60,17 +73,12 @@ public class MenuController {
             table.addCell(String.valueOf(tour.getTourDistance()));
             table.addCell(String.valueOf(tour.getEstimatedTime()));
             table.addCell(tour.getRouteInformation());
-
-            document.add(table);
-
         }
-
+        document.add(table);
         document.close();
     }
 
-    @FXML
-    void initialize() {
-        searchTextField.textProperty().bindBidirectional(menuViewModel.searchStringProperty() );
-        searchTextField.textProperty().addListener((observable, oldValue, newValue) ->  menuViewModel.onSearch());
+    public void onClose() {
+        ((Stage) menuBar.getScene().getWindow()).close();
     }
 }
